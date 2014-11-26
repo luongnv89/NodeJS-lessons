a1dbab93a319da4203a9a17b497466e03259ca10
Nguyen Van Luong <luongnv89@gmail.com>
2014-11-26 01:34:17 +0100

    Add some new route

--- angulajs-jade/public/javascripts/app.js
+++ angulajs-jade/public/javascripts/app.js
@@ -5,15 +5,27 @@ var radioApp = angular.module('radioApp', [
 
 radioApp.config(['$routeProvider',function ($routeProvider) {
   $routeProvider.
-  when('/radios', {
-    templateUrl:'partials/radio-list.jade',
-    controller:'RadioListCtrl'
+  when('/list', {
+    templateUrl:'partials/listradio.jade',
+    controller:'ListRadioCtrl'
   }).
-  when('/radios/:radioId',{
-    templateUrl:'partials/radio-detail.jade',
-    controller:'RadioDetailCtrl'
+  when('/view/:radioId',{
+    templateUrl:'partials/viewradio.jade',
+    controller:'ViewRadioCtrl'
+  }).
+  when('/add',{
+    templateUrl:'partials/addradio.jade',
+    controller:'AddRadioCtrl'
+  }).
+  when('/edit/:radioId',{
+    templateUrl:'partials/editradio.jade',
+    controller:'EditRadioCtrl'
+  }).
+  when('/delete/:radioId',{
+    templateUrl:'partials/deleteradio.jade',
+    controller:'DeleteRadioCtrl'
   }).
   otherwise({
-    redirectTo:'/radios'
+    redirectTo:'/list'
   });
 }]);
--- angulajs-jade/public/javascripts/controller.js
+++ angulajs-jade/public/javascripts/controller.js
@@ -1,6 +1,6 @@
 var radioControllers = angular.module('radioControllers',[]);
 
-radioControllers.controller('RadioListCtrl',['$scope','$http',
+radioControllers.controller('ListRadioCtrl',['$scope','$http',
   function ($scope,$http) {
     $http.get('/allStreams.json').success(function (data) {
       console.log(data);
@@ -8,9 +8,23 @@ radioControllers.controller('RadioListCtrl',['$scope','$http',
     });
   }]);
 
-radioControllers.controller('RadioDetailCtrl',['$scope','$routeParams',function ($scope,$routeParams) {
+radioControllers.controller('ViewRadioCtrl',['$scope','$routeParams',function ($scope,$routeParams) {
   console.log($routeParams.radioId);
   $scope.radioId=$routeParams.radioId;
+}]);
+
+radioControllers.controller('EditRadioCtrl',['$scope','$routeParams',function ($scope,$routeParams) {
+  console.log($routeParams.radioId);
+  $scope.radioId=$routeParams.radioId;
+}]);
+
+radioControllers.controller('DeleteRadioCtrl',['$scope','$routeParams',function ($scope,$routeParams) {
+  console.log($routeParams.radioId);
+  $scope.radioId=$routeParams.radioId;
+}]);
+
+radioControllers.controller('AddRadioCtrl',['$scope',function ($scope,$routeParams) {
+  $scope.test='Hehe';
 }]);
 
 
